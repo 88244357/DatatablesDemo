1e50557c89356aee83dca6887ef822327593e2c9
Li Lei <leil@hpe.com>
2018-07-07 09:01:07 +0800

    update

--- dubbo-provider/src/java/com/alibaba/dubbo/demo/impl/DemoServiceImpl.java
+++ dubbo-provider/src/java/com/alibaba/dubbo/demo/impl/DemoServiceImpl.java
@@ -8,9 +8,9 @@ import java.util.List;
 public class DemoServiceImpl implements DemoService {
     public List<String> getPermissions(Long id) {
         List<String> demo = new ArrayList<String>();
-        demo.add(String.format("Permission_%d", id - 1));
-        demo.add(String.format("Permission_%d", id));
-        demo.add(String.format("Permission_%d", id + 1));
+        demo.add(String.format("*Permission_%d", id - 1));
+        demo.add(String.format("*Permission_%d", id));
+        demo.add(String.format("*Permission_%d", id + 1));
         return demo;
     }
 }
